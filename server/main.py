8952526198587752133c596b56ab0a0483e97e06
Sanket <sgpai2@ncsu.edu>
2020-09-16 19:16:06 -0400

    Create React App

--- server/main.py
+++ server/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/index', methods=['get', 'post'])
 def index():
-    return 'Hello World'
+    return '<br>&nbspHello World, Flask lives.'
 
 
 if __name__ == '__main__':
