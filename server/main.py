7083437eb700a5bc5fc910d18220a9dbb557abb8
Sanket <sgpai2@ncsu.edu>
2020-09-21 18:02:17 -0400

    Add Directory Structure

--- server/main.py
+++ server/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/index', methods=['get', 'post'])
 def index():
     return '<br>&nbspHello World, Flask lives.'
+
+@app.route('/', methods=['get', 'post'])
+def landing():
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
