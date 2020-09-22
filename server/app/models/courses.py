8fd181948e57ee2eb6335f13430bf38bfbd17c05
Sahil <ppapalkar.sahil@gmail.com>
2020-09-22 13:02:17 -0400

    Create course model

---
+++ server/app/models/courses.py
@@ -0,0 +1,13 @@
+from app import db
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    active = db.Column(db.Boolean)
+    users = db.relationship('User', backref='courses')
+
+    def __init__(self, name, active=True):
+        self.name = name
+        self.active = active
--- server/app/models/users.py
+++ server/app/models/users.py
@@ -1,14 +1,17 @@
 from app import db
+from app.models.courses import Course
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    email = db.Column(db.String(64), index = True, unique=True)
-    password = db.Column(db.String(128))
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id", ondelete='CASCADE'), nullable = False)
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False, index = True, unique=True)
+    password = db.Column(db.String(128), nullable=False)
 
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
+    
