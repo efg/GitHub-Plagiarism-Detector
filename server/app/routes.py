0f9e0295409e6262d0f562a1edb05511a61a6f06
Sahil <ppapalkar.sahil@gmail.com>
2020-09-30 20:23:15 -0400

    Add new course route

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,4 +1,7 @@
+from flask import request
 from app import get_app
+from app.utils.response import make_response
+from app.controllers.courses_controller import CourseController
 
 app = get_app()
 
@@ -6,3 +9,14 @@ app = get_app()
 @app.route('/index', methods=['get', 'post'])
 def index():
     return '<br>&nbspHello World, Flask lives.'
+
+@app.route('/course/new', methods=['get', 'post'])
+def course_new():
+    try:
+        CourseController.new(request.args)
+    except ValueError as e: 
+        return make_response(e.args), 400
+    except Exception as e:
+        return make_response('Server Error'), 500
+
+    return make_response('Success'), 200
