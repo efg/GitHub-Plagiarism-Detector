32641654d8c55688e2f19a5b7e8f6fddb1e65311
Sahil <ppapalkar.sahil@gmail.com>
2020-11-19 14:56:02 -0500

    Add style to login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './login.css';
 
 class Login extends Component{
     constructor(props){
@@ -8,12 +9,12 @@ class Login extends Component{
     render(){
         return(
             <div class="container">
-                <form class="sign-in-form" method="post" action="">
+                <form class="sign-in-form" method="post" action="#">
                     <div class="row flex-center min-vh-50 py-6">
                         <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5">
                             <div class="card">
                                 <div class="card-header">
-                                    <div class="col-auto">
+                                    <div class="col-auto fancy-font">
                                         Sign In
                                     </div>
                                 </div>
@@ -29,6 +30,15 @@ class Login extends Component{
                                     </div>
                                     <div class="form-group">
                                         <button class="btn btn-primary btn-block mt-3" type="submit">Sign in  </button>
+                                    </div>
+                                    <div class="w-100 position-relative mt-4">
+                                        <hr class="text-300" />
+                                        <div class="position-absolute absolute-centered t-0 px-3 bg-white text-sans-serif fs--1 text-500 text-nowrap">or</div>
+                                    </div>
+                                    <div class="form-group mb-0">
+                                        <div class="row no-gutters">
+                                            <div class="col-sm-12"><a class="btn btn-falcon-primary btn-block mt-2" href="#"><span class="fab fa-google-plus-g mr-2" data-fa-transform="grow-8"></span> Create an account</a></div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
