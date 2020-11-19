817c5876d4b9bf87606e530de114fb4f70c0baa1
Sahil <ppapalkar.sahil@gmail.com>
2020-11-19 17:26:52 -0500

    Changed to controlled component

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,12 +4,24 @@ import './login.css';
 class Login extends Component{
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            email:"",
+            password:""
+        };
+    }
+    handleChange=event=>{
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+          [name]: value
+        });
+        console.log(this.state);
     }
     render(){
         return(
             <div class="container">
-                <form class="sign-in-form" method="post" action="#">
+                <form class="sign-in-form">
                     <div class="row flex-center min-vh-50 py-6">
                         <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5">
                             <div class="card">
@@ -21,12 +33,12 @@ class Login extends Component{
                                 <div class="card-body p-4 p-sm-5">
                                     <div class="form-group">
                                         {/* <label for="inputEmail" class="text-left">Email address</label> */}
-                                        <input class="form-control" type="email" name="email" id="inputEmail" placeholder="Email address" />
+                                        <input class="form-control" type="email" name="email" id="inputEmail" placeholder="Email address" onChange={this.handleChange} />
                                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                     </div>
                                     <div class="form-group">
                                         {/* <label for="inputPassword">Password</label> */}
-                                        <input class="form-control" type="password" name="password" id="inputPassword" placeholder="Password" />
+                                        <input class="form-control" type="password" name="password" id="inputPassword" placeholder="Password" onChange={this.handleChange} />
                                     </div>
                                     <div class="form-group">
                                         <button class="btn btn-primary btn-block mt-3" type="submit">Sign in  </button>
@@ -37,7 +49,7 @@ class Login extends Component{
                                     </div>
                                     <div class="form-group mb-0">
                                         <div class="row no-gutters">
-                                            <div class="col-sm-12"><a class="btn btn-falcon-primary btn-block mt-2" href="#"><span class="fab fa-google-plus-g mr-2" data-fa-transform="grow-8"></span> Create an account</a></div>
+                                            <div class="col-sm-12"><a class="btn btn-falcon-primary btn-block mt-2" href="#"><span class="fab fa-google-plus-g mr-2" data-fa-transform="grow-8"></span> Request an account</a></div>
                                         </div>
                                     </div>
                                 </div>
