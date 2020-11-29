a48b6b32ac7e94a69decb999161019f576106148
Sahil <ppapalkar.sahil@gmail.com>
2020-11-29 17:55:17 +0530

    Update new course modal

---
+++ src/components/dashboard/courseview/coursemodal.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default class CourseModal extends Component {
+    constructor(props){
+        super(props);
+    }
+    
+    render() {   
+      return (
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">New Course</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body p-4 p-sm-5">
+                        <div class="form-group">
+                            <input class="form-control" type="text" name="name" id="inputName" placeholder="Course Name" onChange={this.props.handleChange} />
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" onClick={this.props.onSubmit}>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+    }
+}
--- src/components/dashboard/courseview/courseview.js
+++ src/components/dashboard/courseview/courseview.js
@@ -1,93 +1,82 @@
-import React, { Component } from 'react';
-import Card from '../card/card'; 
-import Modal from './modal';
-import axios from 'axios';
+import React, { Component } from "react";
+import Card from "../card/card";
+import CourseModal from "./coursemodal";
+import axios from "axios";
 
-class CourseView extends Component{
-    constructor(props){
-        super(props);
-        // console.log(props.courses);
-        this.getCard = this.getCard.bind(this);
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.state = {
-            showModal : false,
-            name: "",
-            user_id: localStorage.getItem('user_id')
-        };
-    }
+class CourseView extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(props.courses);
+    this.getCard = this.getCard.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      name: "",
+      user_id: localStorage.getItem("user_id"),
+    };
+  }
 
-    
+  getCard(course) {
+    return (
+      <Card
+        id={course.id}
+        name={course.name}
+        infoAttr="Course Status"
+        infoVal={course.active === true ? "Active" : "Not Active"}
+        onViewPress={this.props.getChecks}
+      />
+    );
+  }
+  onFormSubmit = async () => {
+    // console.log(this.state);
+    const name = this.state.name;
+    const user_id = this.state.user_id;
 
-    showModalHandler = (event) =>{
-        this.setState({showModal:true});
-    }
-
-    hideModalHandler = (event) =>{
-        this.setState({showModal:false});
-    }
-      
-    getCard(course){
-        return  (
-            <Card id={course.id}
-                  name={course.name} 
-                  infoAttr="Course Status"
-                  infoVal={(course.active === true)? "Active": "Not Active"}
-                  onViewPress={this.props.getChecks}
-            />);
-    }
-    onFormSubmit= async ()=>{
-        // console.log(this.state);
-        this.hideModalHandler();
-        const name = this.state.name;
-        const user_id = this.state.user_id;
-
-       await axios.post('http://127.0.0.1:5000/course/new', { name, user_id })
-      .then(res => {
+    await axios
+      .post("http://127.0.0.1:5000/course/new", { name, user_id })
+      .then((res) => {
         window.location.reload();
-        
       })
       .catch((error) => {
         console.log(error.message);
-        // useHistory().push("/dashboard");
       });
-    }
+  };
 
-    handleChange=event=>{
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState({
-          [name]: value
-        });
-    }
-    render(){
-        return(
-            <div>
-            <div class="container">
-                <div class="card overflow-hidden mb-3">
-                    <div class="card-body p-2">
-                        <div class="row justify-content-between align-items-center pd">
-                            <div class="col">
-                                Courses
-                            </div>
-                            <div class="col">
-                                <button class="btn btn-falcon-primary float-right" onClick={this.showModalHandler}>New Course</button>
-                                
-                            </div>
-                        </div>
-                    </div>
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div class="container">
+          <div class="card overflow-hidden mb-3">
+            <div class="card-body p-2">
+              <div class="row justify-content-between align-items-center pd">
+                <div class="col">Courses</div>
+                <div class="col">
+                    <button type="button" class="btn btn-falcon-primary float-right" data-toggle="modal" data-target="#exampleModal">
+                        New Course
+                    </button>
                 </div>
-                <div class="row mr-1">
-                    {this.props.courses.map((course) => this.getCard(course))}
-                </div>
-                
+              </div>
             </div>
-            <div>
-            <Modal showModal={this.state.showModal} hideModalHandler={this.hideModalHandler} onSubmit={this.onFormSubmit} handleChange={this.handleChange}></Modal>
-            </div>
-            </div>
-        );
-    }
+          </div>
+          <div class="row mr-1">
+            {this.props.courses.map((course) => this.getCard(course))}
+          </div>
+        </div>
+        <CourseModal
+          onSubmit={this.onFormSubmit}
+          handleChange={this.handleChange}
+        ></CourseModal>
+        
+      </div>
+    );
+  }
 }
 export default CourseView;
