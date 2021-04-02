e3ff837827a554d058fa0b12c2b463c3db156816
ssp <2015suraj.pawar@ves.ac.in>
2021-04-01 21:18:42 -0400

    add faulty report error msg on UI

--- src/components/dashboard/card/card.js
+++ src/components/dashboard/card/card.js
@@ -1,38 +1,48 @@
-import React, { Component } from 'react';
-import './card.css';
-
-class Card extends Component{
-    render(){
-        return(
-            <div className="col-sm-3 pr-0">
-                <div className="card mb-3 shadow-sm">
-                    <div className="card-body">
-                        <h6 className="pb-0">{this.props.name}</h6>
-                        <p className="card-text pb-1"><small className="text-muted">{this.props.infoAttr}: {this.props.infoVal}</small></p>           
-                    </div>
-                    <div className="card-body fs--1 pt-0">
-                        <div className="list-group-flush">
-                            <div className="list-group-item bg-transparent d-flex justify-content-between px-0 py-1">
-                                <p className="card-text p-0 card-text-muted">{this.props.attr} {this.props.attrVal}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-footer bg-light p-0">
-                        <button 
-                            className="btn btn-sm btn-link" 
-                            onClick={() => this.props.onViewPress(this.props.id)}>
-                            View
-                        </button>
-                        <button 
-                            className="btn btn-sm btn-link" 
-                            onClick={() => this.props.onDeletePress(this.props.id, this.props.course_id)}>
-                            Delete
-                        </button>
-                    </div>
-                </div>
+import React, { Component } from "react";
+import "./card.css";
+const moment = require('moment')
+class Card extends Component {
+  render() {
+    return (
+      <div className="col-sm-3 pr-0">
+        <div className="card mb-3 shadow-sm">
+          <div className="card-body">
+            <h6 className="pb-0">{this.props.name}</h6>
+            <p className="card-text pb-1">
+              <small className="text-muted">
+                {this.props.infoAttr}: {moment(this.props.infoVal).format('MM/DD/YYYY')}
+              </small>
+            </p>
+          </div>
+          <div className="card-body fs--1 pt-0">
+            <div className="list-group-flush">
+              <div className="list-group-item bg-transparent d-flex justify-content-between px-0 py-1">
+                <p className="card-text p-0 card-text-muted">
+                  {this.props.attr} {this.props.attrVal}
+                </p>
+              </div>
             </div>
-        );
-    }
+          </div>
+          <div className="card-footer bg-light p-0">
+            <button
+              className="btn btn-sm btn-link"
+              onClick={() => this.props.onViewPress(this.props.id)}
+            >
+              View
+            </button>
+            <button
+              className="btn btn-sm btn-link"
+              onClick={() =>
+                this.props.onDeletePress(this.props.id, this.props.course_id)
+              }
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Card;
--- src/components/dashboard/checkview/listview.js
+++ src/components/dashboard/checkview/listview.js
@@ -1,61 +1,70 @@
 import React, { Component } from "react";
-class ListView extends Component{
-    constructor(props) {
-        super(props);
-        this.count = 0;
-        this.getTabRow = this.getTabRow.bind(this);
-    };
-    getTabRow(tabRow){
-        this.count += 1;
-        const report = (tabRow.status)? <a href={tabRow.report} target="_blank">View</a> : <div></div>;
-        if(tabRow.status){
-
-        }
-        return(
-            <tr key={this.count}>
-                <th scope="row">{this.count}</th>
-                <td>{tabRow.date}</td>
-                <td>{(tabRow.status)? "Complete": "InComplete"}</td>
-                <td>{report}</td>
-            </tr>
-        )
+class ListView extends Component {
+  constructor(props) {
+    super(props);
+    this.count = 0;
+    this.getTabRow = this.getTabRow.bind(this);
+  }
+  getTabRow(tabRow) {
+    this.count += 1;
+    const report = tabRow.status && tabRow.report.includes('/') ? (
+      <a href={tabRow.report} target="_blank">
+        View
+      </a>
+    ) : (
+      <div>Error</div>
+    );
+    if (tabRow.status) {
     }
-    render(){
-        return(
-            <div class="container">
-                <div class="card overflow-hidden mb-3">
-                    <div class="card-body p-2">
-                        <div class="row justify-content-between align-items-center pd">
-                            <div class="col">Checks</div>
-                            <div class="col">
-                                <button
-                                    type="button"
-                                    class="btn btn-falcon-primary float-right"
-                                    onClick={this.props.onBackPress}
-                                >
-                                    Back
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row mr-1">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Date</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Report</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.props.tabRows? (this.props.tabRows.map((tabRow) => this.getTabRow(tabRow))) : <p>No Reports Found</p>}
-                        </tbody>
-                    </table>
-                </div>
+    return (
+      <tr key={this.count}>
+        <th scope="row">{this.count}</th>
+        <td>{tabRow.date}</td>
+        <td>{tabRow.status ? "Complete" : "In Complete"}</td>
+        <td>{report}</td>
+      </tr>
+    );
+  }
+  render() {
+    return (
+      <div class="container">
+        <div class="card overflow-hidden mb-3">
+          <div class="card-body p-2">
+            <div class="row justify-content-between align-items-center pd">
+              <div class="col">Checks</div>
+              <div class="col">
+                <button
+                  type="button"
+                  class="btn btn-falcon-primary float-right"
+                  onClick={this.props.onBackPress}
+                >
+                  Back
+                </button>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+        <div class="row mr-1">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Date</th>
+                <th scope="col">Status</th>
+                <th scope="col">Report</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.tabRows ? (
+                this.props.tabRows.map((tabRow) => this.getTabRow(tabRow))
+              ) : (
+                <p>No Reports Found</p>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 export default ListView;
